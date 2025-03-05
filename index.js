a4909fff63ef0fa97d8965885daab42f9932f5d8
Bhim Magar <shokokomichan098@gmail.com>
2025-03-05 16:10:34 +0545

    sixth commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     dots.forEach((dot, index) => {
         dot.addEventListener('click', () => {
             showCard(index);
-        });
+        }); 
     });
     
     // Auto rotate every 5 seconds
